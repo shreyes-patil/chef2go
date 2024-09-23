a30ef17aa703283d912c0cb2a6e02bf02a9ac033
shreyaspatil <patil.shreyas@northeastern.edu>
2024-09-22 22:34:49 -0400

    moved keys to env

--- chef-2-go-backend/utilities/emailHandler.js
+++ chef-2-go-backend/utilities/emailHandler.js
@@ -4,21 +4,14 @@ import * as dotenv from 'dotenv/config';
 
 const sendEmail = async (senderEmail, otp) => {
   try {
-
-    const CLIENT_ID = "494767139204-5p1s4771192p08obvbnfol0njnlabfqg.apps.googleusercontent.com";
-    const CLIENT_SECRET = "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";
-    const REDIRECT_URL = "https://developers.google.com/oauthplayground";
-    const REFRESH_TOKEN = "1//04dFGZYG7FwSBCgYIARAAGAQSNwF-L9IrT5ec6I4g54SWKpSzHDdISI6JMWw0o6RRssGGpso10Q--dUGOWqvi6-_NO9EhJY1oP78";
-    const SENDER_EMAIL = "patilbasavaraj293@gmail.com";
-
     const oauth2Client = new google.auth.OAuth2(
-        CLIENT_ID,
-        CLIENT_SECRET,
-        REDIRECT_URL
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      process.env.REDIRECT_URL
     );
 
     oauth2Client.setCredentials({
-        refresh_token: REFRESH_TOKEN,
+      refresh_token: process.env.REFRESH_TOKEN,
     });
 
     const accessToken = await oauth2Client.getAccessToken();
@@ -27,16 +20,16 @@ const sendEmail = async (senderEmail, otp) => {
       service: 'gmail',
       auth: {
         type: 'OAuth2',
-        user: SENDER_EMAIL,// process.env.USER_EMAIL,
-        clientId: CLIENT_ID,// process.env.CLIENT_ID,
-        clientSecret: CLIENT_SECRET,//process.env.CLIENT_SECRET,
-        refreshToken: REFRESH_TOKEN,//1234 process.env.REFRESH_TOKEN,
+        user: process.env.SENDER_EMAIL,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN,
         accessToken: accessToken.token,
       },
     });
 
     const mailOptions = {
-      from: `CHEF2GO <${SENDER_EMAIL}>`,// process.env.USER_EMAIL,
+      from: `CHEF2GO <${process.env.SENDER_EMAIL}>`,
       to: senderEmail,
       subject: 'Email Verification for CHEF2GO',
       html: `<h2>Welcome to CHEF2GO!</h2><p>Thanks for signing up at CHEF2GO. Your OTP is <b>${otp}</b>, it will expire in 2 minutes.<br></p><h3>See you soon!</h3>`,
